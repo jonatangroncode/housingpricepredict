fd9f7fe773297eb269c53aaf0a3f71ab917dd274
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-11-27 16:53:57 +0100

    feat: Convert yes/no values to binary using pandas map()
    Changed the following features to binary values:
    mainroad, guestroom, basement, hotwaterheating, airconditioning, prefarea.
    
    Initially used replace() but switched to map() due to FutureWarnings about deprecated behavior.
    
    Added a validation loop to ensure all binary features only contain 0 and 1 as values.

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -3,8 +3,26 @@ import pandas as pd
 
 df = pd.read_csv('../data/housing_data.csv')
 
+
+columns_to_convert = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea']
+
+for col in columns_to_convert:
+    df[col] = df[col].map({'yes': 1, 'no': 0})
+
+
 print(df.head())
 print(df.info())
-print(df.describe())
+print(df.tail())
+print(df.info())
+
+for col in columns_to_convert:
+    print(f"uniquee values {col}: {df[col].unique()}")
 
 
+df.to_csv('../data/cleaning_data_step1.csv', index=False)
+
+
+#todo 
+#1 convert variables to binary format 
+#2 normalize area 
+#3 maybe remove some colums 
